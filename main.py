d849649e704cb68f014b29ea4c67c6586d4cd6c2
Nioboy007 <161853537+Nioboy007@users.noreply.github.com>
2024-05-28 10:22:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -47,13 +47,13 @@ Hello! I am a bot to download videos from terabox.
 Send me the terabox link and I will start downloading it.
 Join @RoldexVerse For Updates
 [Source Code](https://github.com/r0ld3x/terabox-downloader-bot) """
-    check_if = await is_user_on_chat(bot, "@RoldexVerse", m.peer_id)
+    check_if = await is_user_on_chat(bot, "@botio_devs", m.peer_id)
     if not check_if:
-        return await m.reply("Please join @RoldexVerse then send me the link again.")
-    check_if = await is_user_on_chat(bot, "@RoldexVerseChats", m.peer_id)
+        return await m.reply("Please join @botio_devs then send me the link again.")
+    check_if = await is_user_on_chat(bot, "@botio_devs", m.peer_id)
     if not check_if:
         return await m.reply(
-            "Please join @RoldexVerseChats then send me the link again."
+            "Please join @botio_devs then send me the link again."
         )
     await m.reply(reply_text, link_preview=False, parse_mode="markdown")
 
@@ -69,13 +69,13 @@ Join @RoldexVerse For Updates
 async def start(m: UpdateNewMessage):
     text = m.pattern_match.group(1)
     fileid = db.get(str(text))
-    check_if = await is_user_on_chat(bot, "@RoldexVerse", m.peer_id)
+    check_if = await is_user_on_chat(bot, "@botio_devs", m.peer_id)
     if not check_if:
-        return await m.reply("Please join @RoldexVerse then send me the link again.")
-    check_if = await is_user_on_chat(bot, "@RoldexVerseChats", m.peer_id)
+        return await m.reply("Please join @botio_devs then send me the link again.")
+    check_if = await is_user_on_chat(bot, "@botio_devs", m.peer_id)
     if not check_if:
         return await m.reply(
-            "Please join @RoldexVerseChats then send me the link again."
+            "Please join @botio_devs then send me the link again."
         )
     await bot(
         ForwardMessagesRequest(
@@ -126,13 +126,13 @@ async def handle_message(m: Message):
     url = get_urls_from_string(m.text)
     if not url:
         return await m.reply("Please enter a valid url.")
-    check_if = await is_user_on_chat(bot, "@RoldexVerse", m.peer_id)
+    check_if = await is_user_on_chat(bot, "@botio_devs", m.peer_id)
     if not check_if:
-        return await m.reply("Please join @RoldexVerse then send me the link again.")
-    check_if = await is_user_on_chat(bot, "@RoldexVerseChats", m.peer_id)
+        return await m.reply("Please join @botio_devs then send me the link again.")
+    check_if = await is_user_on_chat(bot, "@botio_devs", m.peer_id)
     if not check_if:
         return await m.reply(
-            "Please join @RoldexVerseChats then send me the link again."
+            "Please join @botio_devs then send me the link again."
         )
     is_spam = db.get(m.sender_id)
     if is_spam and m.sender_id not in [1317173146]:
