2e5b1c034d2a9919709a0563159b08ab3930d58b
Nioboy007 <161853537+Nioboy007@users.noreply.github.com>
2024-05-28 11:05:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,10 +5,10 @@ BOT_TOKEN = "123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11"  # bot token
 
 
 ## REDIS
-HOST = "localhost"  # redis host uri
-PORT = 6379  # redis port
-PASSWORD = "hq7oWfQBhnhrXpFTMXHNlGWqYbOFA3JT"  # redis password
+HOST = "redis-18689.c85.us-east-1-2.ec2.redns.redis-cloud.com"  # redis host uri
+PORT = 18689  # redis port
+PASSWORD = "5bQedh5WnTg24cMblDGeRjylvQxT1cBx"  # redis password
 
-PRIVATE_CHAT_ID = -1001234567890  # CHAT WHERE YOU WANT TO STORE VIDEOS
-COOKIE = ""  # COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=; lang=en; TSID=; __bid_n=; _ga=; __stripe_mid=; ndus=; browserid==; ndut_fmt=; _ga_06ZNKL8C2E=" (don't use this)
-ADMINS = [1317173146]
+PRIVATE_CHAT_ID = -1002089570434 # CHAT WHERE YOU WANT TO STORE VIDEOS
+COOKIE = "none"  # COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=; lang=en; TSID=; __bid_n=; _ga=; __stripe_mid=; ndus=; browserid==; ndut_fmt=; _ga_06ZNKL8C2E=" (don't use this)
+ADMINS = [6883997969]
